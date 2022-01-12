447e339238289dade9d2f677bff09fca33870bf8
clarajusta <clarajusta31@gmail.com>
2022-01-11 22:15:18 -0300

    print, string concatenation, input, variable

---
+++ Exercises/Course 1/First/main.py
@@ -0,0 +1,40 @@
+# Exercise 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Exercise 2
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Exercise 3
+print(len(input("What is your name?")))
+
+# Exercise 4
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+b = c
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+# Day 1 Project
+
+print("Welcome to the Band Name Generator")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
+
